85c66b26d4f57b06bf6d878a665e896e691ecfb0
WallisonLucas13 <lucaswalison136@gmail.com>
2025-03-26 14:24:04 -0300

    feat: implements doc with Swagger

--- src/main/java/com/example/transfer/api/TransferApiApplication.java
+++ src/main/java/com/example/transfer/api/TransferApiApplication.java
@@ -1,9 +1,12 @@
 package com.example.transfer.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API de Transferências", version = "1.0", description = "API de gerenciamento e transferências financeiras"))
 public class TransferApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/transfer/api/dtos/TransferDto.java
+++ src/main/java/com/example/transfer/api/dtos/TransferDto.java
@@ -1,14 +1,27 @@
 package com.example.transfer.api.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
 public record TransferDto(
-        @NotNull @Positive @JsonProperty(required = true) BigDecimal value,
-        @NotNull @Positive @JsonProperty(value = "payer", required = true) Long payerId,
-        @NotNull @Positive @JsonProperty(value = "payee", required = true) Long payeeId
+        @NotNull
+        @Positive
+        @JsonProperty(required = true)
+        @Schema(description = "Valor da transferência", example = "100.00")
+        BigDecimal value,
+        @NotNull
+        @Positive
+        @JsonProperty(value = "payer", required = true)
+        @Schema(description = "ID do pagador", example = "1")
+        Long payerId,
+        @NotNull
+        @Positive
+        @JsonProperty(value = "payee", required = true)
+        @Schema(description = "ID do recebedor", example = "2")
+        Long payeeId
 ) {
 }
