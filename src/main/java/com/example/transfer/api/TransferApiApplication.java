16906c30cc33bc581fdfcbceb504c9ac9be917ae
WallisonLucas13 <lucaswalison136@gmail.com>
2025-03-26 14:35:25 -0300

    feat: implements observability with Actuator

--- src/main/java/com/example/transfer/api/TransferApiApplication.java
+++ src/main/java/com/example/transfer/api/TransferApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "API de Transferências", version = "1.0", description = "API de gerenciamento e transferências financeiras"))
+@OpenAPIDefinition(info = @Info(title = "API de Transferências", version = "1.0", description = "API para transferências financeiras"))
 public class TransferApiApplication {
 
 	public static void main(String[] args) {
