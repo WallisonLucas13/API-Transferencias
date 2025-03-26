6095cdd02fc93acf0731e2b76804d8ea0db26f4c
WallisonLucas13 <lucaswalison136@gmail.com>
2025-03-26 11:47:30 -0300

    feat: implements transfer controller

---
+++ src/main/java/com/example/transfer/api/controllers/TransferController.java
@@ -0,0 +1,24 @@
+package com.example.transfer.api.controllers;
+
+import com.example.transfer.api.dtos.TransferDto;
+import com.example.transfer.api.services.TransferService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transfer")
+@RequiredArgsConstructor
+public class TransferController {
+
+    private final TransferService transferService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Void> transfer(@Valid @RequestBody TransferDto transferDto) {
+        transferService.transferMoney(transferDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
